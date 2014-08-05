d409d03782b1135871ed782a736a71d98abc1c89
Sam Schwartz <obct537@gmail.com>
2014-08-05 12:35:06 -0500

    Fixed useless uninstall profile

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version='0.8.9b'
+version='0.9.01'
 
 long_description = (
     open('README.rst').read()
---
+++ src/uwosh/snippets/Extensions/Install.py
@@ -0,0 +1,16 @@
+from Products.CMFCore.utils import getToolByName
+from zope.component.hooks import getSite
+
+def uninstall(self, reinstall=False):
+	if not reinstall:
+		ps = getToolByName(self, 'portal_setup')
+
+		site = getSite()
+		pt = getToolByName(site, 'portal_tinymce')
+
+		ps.runAllImportStepsFromProfile('profile-uwosh.snippets:uninstall')
+
+		pt.customplugins = pt.customplugins.replace(u'\nsnippets|++resource++uwosh.snippets/snippets.js', u'')
+		pt.customtoolbarbuttons = pt.customtoolbarbuttons.replace(u'\nsnippetbutton', u'')
+
+		return "Uninstall successful."
