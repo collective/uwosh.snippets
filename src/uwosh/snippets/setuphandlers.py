8b8746780dbb78b8df686added15902a189cb6f1
Sam Schwartz <obct537@gmail.com>
2014-08-07 10:22:36 -0500

    Uninstall profile no longer shows up when creating a new site. Fixes #6

--- src/uwosh/snippets/setuphandlers.py
+++ src/uwosh/snippets/setuphandlers.py
@@ -1,8 +1,19 @@
-__docformat__ = "epytext"
+from Products.CMFPlone.interfaces import INonInstallable
+from zope.interface import implements
+
+class HiddenProfiles(object):
+    implements(INonInstallable)
+
+    def getNonInstallableProfiles(self):
+        """
+		Prevents all profiles but 'default' from showing up in the
+		profile list when creating a Plone site.
+		"""
+        return [
+            u'uwosh.snippets:uninstall',
+        ]
+
 
 def setupVarious(context):
-
-    if not context.readDataFile('uwosh.snippets.marker.txt'):
-        return
 
     site = context.getSite()
