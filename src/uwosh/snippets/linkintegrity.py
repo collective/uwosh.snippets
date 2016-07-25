0f93711b258ea4b56cd8fba6b3b06d39ba24ebbb
vangheem <vangheem@gmail.com>
2016-07-25 09:59:28 -0500

    add link checking for headers that get removed

--- src/uwosh/snippets/linkintegrity.py
+++ src/uwosh/snippets/linkintegrity.py
@@ -1,17 +1,24 @@
-from zope.intid.interfaces import IIntIds
-from z3c.relationfield import RelationValue
-from zope.keyreference.interfaces import NotYet
-from zope.component import getUtility
 from lxml.html import fromstring
-from plone.dexterity.interfaces import IDexterityFTI
+from plone import api
+from plone.app.linkintegrity.handlers import check_linkintegrity_dependencies
+from plone.app.linkintegrity.handlers import getObjectsFromLinks
+from plone.app.linkintegrity.handlers import updateReferences
+from plone.app.linkintegrity.interfaces import IRetriever
+from plone.app.linkintegrity.utils import getIncomingLinks
 from plone.app.textfield import RichText
 from plone.app.uuid.utils import uuidToObject
+from plone.dexterity.interfaces import IDexterityFTI
 from plone.dexterity.utils import getAdditionalSchemata
+from plone.registry.interfaces import IRegistry
+from plone.uuid.interfaces import IUUID
+from Products.CMFCore.Expression import Expression
+from uwosh.snippets.utils import ExpressionEvaluator
+from z3c.relationfield import RelationValue
+from zope.component import getUtility
+from zope.globalrequest import getRequest
+from zope.intid.interfaces import IIntIds
+from zope.keyreference.interfaces import NotYet
 from zope.schema import getFieldsInOrder
-from plone.app.linkintegrity.interfaces import IRetriever
-from plone.app.linkintegrity.handlers import check_linkintegrity_dependencies
-from plone.app.linkintegrity.handlers import updateReferences
-from plone.app.linkintegrity.handlers import getObjectsFromLinks
 
 
 def getSnippetRefsFromHtml(html):
@@ -39,21 +46,73 @@ def getSnippetRefsFromHtml(html):
     return refs
 
 
-def getSnippetRefs(obj):
+def findTextAreas(obj):
     fti = getUtility(IDexterityFTI, name=obj.portal_type)
     schema = fti.lookupSchema()
     additional_schema = getAdditionalSchemata(
         context=obj, portal_type=obj.portal_type)
     schemas = [i for i in additional_schema] + [schema]
-    refs = set()
     for schema in schemas:
         for name, field in getFieldsInOrder(schema):
             if isinstance(field, RichText):
                 value = getattr(schema(obj), name)
                 if not value or not getattr(value, 'raw', None):
                     continue
-                refs |= set(getSnippetRefsFromHtml(value.raw))
+                yield value.raw
+
+
+def getSnippetRefs(obj):
+    refs = set()
+    for text in findTextAreas(obj):
+        refs |= set(getSnippetRefsFromHtml(text))
     return refs
+
+
+def checkSnippetReferences(obj):
+    """
+    Checks if this content is a snippet on any other content.
+    If it is, make sure the header setting is not ruined
+    """
+    registry = getUtility(IRegistry)
+    evaluator = ExpressionEvaluator()
+    expression = Expression(registry.get('uwosh.snippets.render_expression',
+                                         'context/text/output|context/getText|nothing'))
+    obj_headers = []
+    html = evaluator.evaluate(expression, obj)
+    if not html:
+        return
+
+    dom = fromstring(html)
+    for el in dom.cssselect('h1,h2,h3,h4,h5,h6'):
+        obj_headers.append(el.text.strip())
+
+    broken = []
+    for link in getIncomingLinks(obj):
+        for text in findTextAreas(link.from_object):
+            if not text:
+                continue
+
+            dom = fromstring(text)
+            for el in dom.cssselect('[data-snippet-id="{}"]'.format(IUUID(obj))):
+                header = el.attrib.get('data-header')
+                if not header:
+                    continue
+                if header not in obj_headers:
+                    # broken reference
+                    broken.append({
+                        'header': header,
+                        'link': link.from_object
+                    })
+
+    if len(broken) > 0:
+        # show status message warning
+        names = set()
+        for b in broken:
+            names.add('{}:{}'.format(b['link'].absolute_url(), b['header']))
+        message = 'Broken snippet references: {}'.format(
+            ', '.join(names)
+        )
+        api.portal.show_message(message=message, request=getRequest(), type='error')
 
 
 def modifiedDexterity(obj, event):
@@ -66,3 +125,7 @@ def modifiedDexterity(obj, event):
         refs = getObjectsFromLinks(obj, links)
         refs |= getSnippetRefs(obj)
         updateReferences(obj, refs)
+
+    # now, check if THIS object is referenced by others and give user warning if there
+    # are now missing headers that are referenced
+    checkSnippetReferences(obj)
--- src/uwosh/snippets/tests/test_linkintegrity.py
+++ src/uwosh/snippets/tests/test_linkintegrity.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
-from plone.app.linkintegrity.utils import getOutgoingLinks
+from zope.annotation.interfaces import IAnnotations
+from Products.statusmessages import STATUSMESSAGEKEY
+from plone.app.textfield.value import RichTextValue
+from plone.app.linkintegrity.utils import getIncomingLinks, getOutgoingLinks
 from plone.uuid.interfaces import IUUID
+from uwosh.snippets.linkintegrity import checkSnippetReferences
+from uwosh.snippets.linkintegrity import getSnippetRefs
 from uwosh.snippets.testing import BaseTest
 from uwosh.snippets.testing import UWOSH_SNIPPETS_INTEGRATION_TESTING
-from uwosh.snippets.linkintegrity import getSnippetRefs
 
 
 class TestTransform(BaseTest):
@@ -21,3 +25,41 @@ class TestTransform(BaseTest):
         # should also have stored these refs for object
         links = getOutgoingLinks(page2)
         self.assertEqual([l for l in links][0].to_object.getId(), 'test1')
+
+    def test_breaking_header_links(self):
+        page = self._create_page(_id='test1', text='''
+<h1>Foobar 1</h1>
+<p>foobar 1</p>
+<h1>Foobar 2</h1>
+<p>foobar 2</p>
+<h2>Foobar 3</h2>
+<p>foobar 3</p>
+<h1>Foobar 4</h1>
+<p>foobar 4</p>
+''')
+        page2 = self._create_page(_id='test2', text='''
+<div data-type="snippet_tag"
+     data-snippet-id="{}"
+     data-header="Foobar 2"></div>'''.format(IUUID(page)))
+
+        links = getOutgoingLinks(page2)  # should add link
+        self.assertEqual([l for l in links][0].to_object.getId(), 'test1')
+        links = getIncomingLinks(page)  # should add link
+        self.assertEqual([l for l in links][0].from_object.getId(), 'test2')
+
+        # now, remove the header...
+        page.text = RichTextValue('''
+<h1>Foobar 1</h1>
+<p>foobar 1</p>
+<h2>Foobar 3</h2>
+<p>foobar 3</p>
+<h1>Foobar 4</h1>
+<p>foobar 4</p>
+''', 'text/html', 'text/html')
+
+        status_annotations = IAnnotations(self.request)
+        self.assertFalse(bool(status_annotations.get(STATUSMESSAGEKEY)))
+
+        checkSnippetReferences(page)
+
+        self.assertTrue(bool(status_annotations.get(STATUSMESSAGEKEY)))
